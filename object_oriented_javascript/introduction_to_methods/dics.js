326df662e2ddfa5b5115226b3156b8971a8123a8
antwonlee <antwonlee@gmail.com>
2016-05-27 10:11:05 -0500

    Add simple constructor

--- object_oriented_javascript/introduction_to_methods/dics.js
+++ object_oriented_javascript/introduction_to_methods/dics.js
@@ -16,10 +16,23 @@
 //   }
 // }
 
-var dice = {
-  sides: 6,
-  roll:  function() {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1
+// var dice = {
+//   sides: 6,
+//   roll:  function() {
+//     var randomNumber = Math.floor(Math.random() * this.sides) + 1
+//     return randomNumber;
+//   }
+// }
+
+// Simple Constructor
+
+function Dice(sides) {
+  this.sides = sides;
+  this.roll = function() {
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     return randomNumber;
   }
 }
+
+var dice = new Dice(6);
+

123f762a7345480b91c6f3f10fdeaf28271c2d60
antwonlee <antwonlee@gmail.com>
2016-06-01 01:12:05 -0500

    Add playlist project practice

---
+++ object_oriented_javascript/playlist/app.js
@@ -0,0 +1,7 @@
+var playlist = new Playlist();
+
+var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Wave", "3:43");
+
+playlist.add(hereComesTheSun);
+playlist.add(walkingOnSunshine);
---
+++ object_oriented_javascript/playlist/song.js
@@ -0,0 +1,18 @@
+function Song(title, artist, duration) {
+  this.title = title;
+  this.artist = artist;
+  this.duration = duration;
+  this.isPlaying = false;
+}
+
+Song.prototype.play = function() {
+  this.isPlaying = true;
+};
+
+Song.prototype.stop = function() {
+  this.isPlaying = false;
+};
+
+Song.prototype.toHTML = function() {
+  
+};

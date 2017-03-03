67fa628cd22378e2e68171907d8f21eb04bdce60
antwonlee <antwonlee@gmail.com>
2017-03-03 16:55:39 -0600

    Add component inside another component

--- vuejs/main.js
+++ vuejs/main.js
@@ -1,5 +1,32 @@
+// Vue.component('task-list', {
+//   template: '<li><slot></slot></li>'
+// });
+
+// new Vue({
+//   el: '#root'
+// })
+
 Vue.component('task-list', {
-  template: '<li><slot></slot></li>'
+  template: `
+    <div>
+      <task v-for="task in tasks">{{ task.task }}</task>
+    </div>
+  `,
+
+  data() {
+    return {
+      tasks: [
+        { task: 'Milk', complete: true },
+        { task: 'Chocolate', complete: true },
+        { task: 'Water', complete: true },
+        { task: 'Toothbrush', complete: true }
+      ]
+    };
+  }
+});
+
+Vue.component('task', {
+  template: '<li><slot></slot></li>',
 });
 
 new Vue({

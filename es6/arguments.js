8a0f6a1dacc689e238c0888d4dcd0a890d124b14
Anthony Lee <antwonlee@gmail.com>
2017-05-03 03:37:18 -0500

    Add simple arrow function expression examples

---
+++ es6/arguments.js
@@ -0,0 +1,43 @@
+const square = x => x * x;
+
+const cube = x => square(x) * x;
+
+const add = (x, y) => {
+  return x + y;
+}
+
+const add = (x, y) => x + y;
+
+
+const multiply = (x, y) => {
+  return x * y;
+}
+
+const multiply = (x, y) => x * y;
+
+
+const subtract = (x, y) => {
+  return x - y;
+}
+
+const subtract = (x, y) => x - y;
+
+// function declaration
+function divide1 (a, b) {
+  return a / b;
+}
+
+// function expression
+const divide2 = function(a, b) {
+  return a / b;
+}
+
+// arrow function expression
+const divide3 = (a, b) => {
+  return a / b ;
+}
+
+// arrow function expression simple
+const divide4 = (a, b) => a / b;
+
+
---
+++ es6/var-scope.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var hello = 'hello';
+
+function sayHi() {
+  var hello = 'hi';
+  console.log(hello);
+}
+
+sayHi();
+console.log(hello);

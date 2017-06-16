32dfc0f696554bef40c8b7fc99397d4e840f06be
antwonlee <antwonlee@gmail.com>
2017-06-16 11:31:50 -0500

    Add data centric vs dom
    + Vue uses this way to interact with the dom

--- vuejs/article.js
+++ vuejs/article.js
@@ -1,16 +1,32 @@
 Vue.component('message', {
   props: ['title', 'body'],
+
+  data() {
+    return {
+      isVisible: true
+    };
+  },
+
   template: `
-  <article class="message">
+  <article class="message" v-show="isVisible">
     <div class="message-header">
       {{ title }}
+
+      <button type="type" @click="hideModal">x</button>
+
     </div>
 
     <div class="message-body">
       {{ body }}
     </div>
   </article>
-  `
+  `,
+
+  methods: {
+    hideModal() {
+      this.isVisible = false;
+    }
+  }
 });
 
 new Vue({

bc1a496694f609c8dae49cdc7411e2375b58041f
Anthony Lee <anthonylee@gatech.edu>
2017-09-01 10:46:34 -0500

    Add template string example

--- es6/extreme-template-string.js
+++ es6/extreme-template-string.js
@@ -1,21 +1,30 @@
 'use strict';
 
-const student = { name: 'James', followerCount: 34 };
+// const student = { name: 'James', followerCount: 34 };
 
-let tableHtml = `
-  <table class="table">
-    <thead>
-      <tr>
-        <td>Name</td>
-        <td>Followers</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>${student.name}</td>
-        <td>${student.followerCount}</td>
-      </tr>
-    </tbody>
-  </table>`;
+// let tableHtml = `
+//   <table class="table">
+//     <thead>
+//       <tr>
+//         <td>Name</td>
+//         <td>Followers</td>
+//       </tr>
+//     </thead>
+//     <tbody>
+//       <tr>
+//         <td>${student.name}</td>
+//         <td>${student.followerCount}</td>
+//       </tr>
+//     </tbody>
+//   </table>`;
 
-console.log(tableHtml);
+// console.log(tableHtml);
+
+const full_name = {
+  first_name: 'Anthony',
+  last_name: 'Lee'
+};
+
+let greeting = `<p>Hi, my name is ${full_name.first_name}.  Nice to meet you!  My last name is ${full_name.last_name}.</p>`;
+
+console.log(greeting);

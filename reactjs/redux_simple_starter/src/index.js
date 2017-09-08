5613f91ecc6fe576c2064294e1a9ffe627d8dc27
Anthony Lee <antwonlee@gmail.com>
2017-09-08 16:44:53 -0500

    Add react import export example

--- reactjs/redux_simple_starter/src/index.js
+++ reactjs/redux_simple_starter/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from 'search_bar'
+import SearchBar from './components/search_bar'
 
 const API = 'AIzaSyBlbDN6ndZUlMl6wQWNhXAZznE7dodCsXQ';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
+

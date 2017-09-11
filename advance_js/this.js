542485a744f1e45e197780edda65188fa30d238d
Anthony Lee <antwonlee@gmail.com>
2017-09-11 18:33:46 -0500

    Add this with constructor

--- advance_js/this.js
+++ advance_js/this.js
@@ -1,8 +1,8 @@
 // go over in depth of this
 
-console.log(this);
+// console.log(this);
 
-this.anthony = 1;
+// this.anthony = 1;
 
 // console.log(this.anthony);
 // console.log(window.anthony);
@@ -13,30 +13,70 @@ this.anthony = 1;
 // }
 // checkThis();
 
-var anthony = {
-  checkThis: function() {
-    // stablelize
+// var anthony = {
+//   checkThis: function() {
+//     // stablelize
 
-    var self = this;
+//     var self = this;
 
-    console.log(self);
+//     console.log(self);
 
-    function checkOther() {
-      console.log(self);
+//     function checkOther() {
+//       console.log(self);
 
-      // this calls window again
-      this.moo = 1 
-    }
+//       // this calls window again
+//       this.moo = 1 
+//     }
 
-    checkOther();
+//     checkOther();
 
-    console.log(self.moo);
-    console.log(window.moo);
-  }
-}
-anthony.checkThis();
+//     console.log(self.moo);
+//     console.log(window.moo);
+//   }
+// }
+// anthony.checkThis();
 // console.log(anthony);
 
 // calls global object again
 // var func = anthony.checkThis;
 // func();
+
+// var Portland = {
+//   bridges: 12,
+//   airport: 1,
+//   soccerTeam: 1,
+//   logNumberOfBridges: function() {
+//     console.log("There are " + this.bridges + " bridges in Portland!")
+//   }
+//   // logTeams: function() {
+//   //   console.log(this.soccerTeam);
+//   // }
+// }
+
+// function logTeams() {
+//   // console.log(this.soccerTeam);
+//   console.log(this);
+// }
+
+// it references the object, when the method is inside the object
+// Portland.logNumberOfBridges();
+// Portland.logTeams();
+
+// Portland.foo = logTeams;
+// Portland.foo();
+
+// logTeams();
+
+
+// Constructor
+
+var City = function(name, state) {
+  this.name = name || 'Portland';
+  this.state = state || 'Oregon';
+};
+
+portland = new City();
+seattle = new City('Seattle', 'Washington');
+
+console.log(portland);
+console.log(seattle);

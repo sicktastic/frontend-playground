1b85b445476e76d7c756979b8deed58080834d0e
Anthony Lee <antwonlee@gmail.com>
2017-09-12 10:08:10 -0500

    Add fibonacci example

---
+++ algorithms/fibonacci.js
@@ -0,0 +1,18 @@
+// http://thatjsdude.com/interview/js1.html
+// Q. How do get nth Fibonnacci number?
+// A. I create an array and start from iterate through.
+// Fibonacci series is one of the most popular interview question for beginners.
+
+function fibonacci(n) {
+  var fibo = [0, 1];
+  
+  if(n <= 2) return 1;
+
+  for(var i =2; i <= n; i++) {
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+  }
+
+  return fibo[n];
+}
+
+console.log(fibonacci(12));

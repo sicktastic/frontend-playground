a3e03895ce1e50f8b1c1767e2a5ddb5a1c7da2db
Anthony Lee <antwonlee@gmail.com>
2017-11-13 15:42:14 -0600

    Ad array list with map

--- reactjs/redux_simple_starter/src/index.js
+++ reactjs/redux_simple_starter/src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyBlbDN6ndZUlMl6wQWNhXAZznE7dodCsXQ';
 
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.set.videos} />
       </div>
     );
   }

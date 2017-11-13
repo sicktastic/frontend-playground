e6237eaaecde475a9798e6369ec827d95b370d42
Anthony Lee <antwonlee@gmail.com>
2017-11-12 22:24:58 -0600

    Add state

--- reactjs/redux_simple_starter/src/components/search_bar.js
+++ reactjs/redux_simple_starter/src/components/search_bar.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return <input onChange={event => this.setState({ term: event.target.value })} />;
   }
 }
 

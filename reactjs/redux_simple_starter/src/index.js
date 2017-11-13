94118d8f7f36e689c0e5f347d531e6397eb0dadb
Anthony Lee <antwonlee@gmail.com>
2017-11-12 22:53:32 -0600

    Add YTSearch

--- reactjs/redux_simple_starter/src/index.js
+++ reactjs/redux_simple_starter/src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyBlbDN6ndZUlMl6wQWNhXAZznE7dodCsXQ';
 
-const API = 'AIzaSyBlbDN6ndZUlMl6wQWNhXAZznE7dodCsXQ';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboard'}, function(videos) {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));

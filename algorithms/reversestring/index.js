48c00c982070e764cb1e9995e6c11b59373e83eb
Anthony Lee <antwonlee@gmail.com>
2017-11-20 16:58:59 -0600

    Add manual solution to reverse()

--- algorithms/reversestring/index.js
+++ algorithms/reversestring/index.js
@@ -6,11 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('')
+// }
+
 function reverse(str) {
-  return str
-    .split('')
-    .reverse()
-    .join('')
+  let reversed = '';
+
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
 }
 
 module.exports = reverse;

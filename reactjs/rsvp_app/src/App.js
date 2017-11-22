e244389458b45f3af48a7bfe58e15c1577eef4c3
Anthony Lee <antwonlee@gmail.com>
2017-11-22 16:02:15 -0600

    Add Guest and GuestList

--- reactjs/rsvp_app/src/App.js
+++ reactjs/rsvp_app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import GuestList from './GuestList'
 
 class App extends Component {
 
@@ -11,6 +12,10 @@ class App extends Component {
       },
       {
         name: "Nick",
+        isConfirmed: true
+      },
+      {
+        name: "Matt",
         isConfirmed: true
       }
     ]
@@ -54,32 +59,9 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+
+          <GuestList guests={this.state.guests} />
+
         </div>
       </div>
     );
---
+++ reactjs/rsvp_app/src/GuestList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Guest from './Guest'
+
+const GuestList = props => 
+  <ul>
+    {props.guests.map((guest, index) =>
+      <Guest key={index} name={guest.name} isConfirmed={guest.isConfirmed} />
+    )} 
+  </ul>;
+
+GuestList.propTypes = {
+  guests: PropTypes.array.isRequired
+}
+
+export default GuestList

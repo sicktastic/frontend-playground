529aea0f41b2a2e4b845747c4e239fd25ac0f3e6
Anthony Lee <antwonlee@gmail.com>
2017-11-22 15:17:24 -0600

    Set up state

--- reactjs/rsvp_app/src/App.js
+++ reactjs/rsvp_app/src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: "Treasure",
+        isConfirmed: false
+      },
+      {
+        name: "Nick",
+        isConfirmed: true
+      }
+    ]
+  }
+
+  getTotalInvited = () => this.state.guests.length;
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
+
   render() {
     return (
       <div className="App">
@@ -10,62 +28,62 @@ class App extends Component {
           <p>A Treehouse App</p>
           <form>
             <input type="text" value="Safia" placeholder="Invite Someone" />
-              <button type="submit" name="submit" value="submit">Submit</button>
-            </form>
-          </header>
-          <div className="main">
-            <div>
-              <h2>Invitees</h2>
+            <button type="submit" name="submit" value="submit">Submit</button>
+          </form>
+        </header>
+        <div className="main">
+          <div>
+            <h2>Invitees</h2>
+            <label>
+              <input type="checkbox" /> Hide those who haven't responded
+            </label>
+          </div>
+          <table className="counter">
+            <tbody>
+              <tr>
+                <td>Attending:</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>Unconfirmed:</td>
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>Total:</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+          <ul>
+            <li className="pending"><span>Safia</span></li>
+            <li className="responded"><span>Iver</span>
               <label>
-                <input type="checkbox" /> Hide those who haven't responded
-                </label>
-              </div>
-              <table className="counter">
-                <tbody>
-                  <tr>
-                    <td>Attending:</td>
-                    <td>2</td>
-                  </tr>
-                  <tr>
-                    <td>Unconfirmed:</td>
-                    <td>1</td>
-                  </tr>
-                  <tr>
-                    <td>Total:</td>
-                    <td>3</td>
-                  </tr>
-                </tbody>
-              </table>
-              <ul>
-                <li className="pending"><span>Safia</span></li>
-                <li className="responded"><span>Iver</span>
-                  <label>
-                    <input type="checkbox" checked /> Confirmed
-                    </label>
-                    <button>edit</button>
-                    <button>remove</button>
-                  </li>
-                  <li className="responded">
-                    <span>Corrina</span>
-                    <label>
-                      <input type="checkbox" checked /> Confirmed
-                      </label>
-                      <button>edit</button>
-                      <button>remove</button>
-                    </li>
-                    <li>
-                      <span>Joel</span>
-                      <label>
-                        <input type="checkbox" /> Confirmed
-                        </label>
-                        <button>edit</button>
-                        <button>remove</button>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                );
-                }
-                }
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li className="responded">
+              <span>Corrina</span>
+              <label>
+                <input type="checkbox" checked /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+            <li>
+              <span>Joel</span>
+              <label>
+                <input type="checkbox" /> Confirmed
+              </label>
+              <button>edit</button>
+              <button>remove</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
 
-                export default App;
+export default App;

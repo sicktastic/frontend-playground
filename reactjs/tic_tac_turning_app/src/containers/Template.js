3fcb5bec8b40c036c34e29c77274f29347d72626
Anthony Lee <antwonlee@gmail.com>
2017-11-22 23:39:32 -0600

    Add material-ui

--- reactjs/tic_tac_turning_app/src/containers/Template.js
+++ reactjs/tic_tac_turning_app/src/containers/Template.js
@@ -1,16 +1,22 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+injectTapEventPlugin()
 
 class Template extends Component {
   render() {
     return(
-      <div>
-        <header>
-          <h1>TicTacTurning</h1>
-        </header>
-        <main>
-          {this.prop.children}
-        </main>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <header>
+            <h1>TicTacTurning</h1>
+          </header>
+          <main>
+            {this.prop.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }

32d30010c7d4721552b035792aeb1d1a0db004ee
Anthony Lee <antwonlee@gmail.com>
2018-02-04 01:46:19 -0600

    Update index.js

--- reactjs/server/index.js
+++ reactjs/server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20'),Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000);

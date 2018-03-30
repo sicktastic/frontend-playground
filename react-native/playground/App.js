9edc3ae2f04804c0340779d8161eb5582698242f
Anthony Lee <antwonlee@gmail.com>
2018-03-30 11:26:45 -0500

    Add buttons with react native

--- react-native/playground/App.js
+++ react-native/playground/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
@@ -15,12 +15,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300}}
-          placeholder="An awesome place"
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangeHandler}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput 
+            placeholder="An awesome place"
+            value={this.state.placeName}
+            onChangeText={this.placeNameChangeHandler}
+            style={styles.placeInput}
+          />
+          <Button title="Add" style={styles.placeButton} />
+        </View>
       </View>
     );
   }
@@ -33,5 +36,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    alignItems: 'center'
   },
+  inputContainer: {
+    // flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  placeInput:  {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });

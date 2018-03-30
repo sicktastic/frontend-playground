50ccc834aec47e5d1049734eb98fc49b34725e54
Anthony Lee <antwonlee@gmail.com>
2018-03-30 16:04:44 -0500

    Ins and outs of render it to device

--- react-native/albums/index.js
+++ react-native/albums/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import ReactNative from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+// Render it to the device
+ReactNative.AppRegistry.registerComponent('albums', () => App);

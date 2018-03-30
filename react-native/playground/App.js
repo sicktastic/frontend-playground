5d10609f18063c9a5e1002c101bffa1ac7654a14
Anthony Lee <antwonlee@gmail.com>
2018-03-30 12:02:09 -0500

    Add button and manage state

--- react-native/playground/App.js
+++ react-native/playground/App.js
@@ -3,7 +3,8 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
   placeNameChangeHandler = val => {
@@ -12,7 +13,20 @@ export default class App extends React.Component {
     });
   }
 
+  placeSubmitHandler = () => {
+    if(this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  }
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => <Text key={i}>{place}</Text>);
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
@@ -22,7 +36,13 @@ export default class App extends React.Component {
             onChangeText={this.placeNameChangeHandler}
             style={styles.placeInput}
           />
-          <Button title="Add" style={styles.placeButton} />
+          <Button 
+            title="Add" 
+            style={styles.placeButton} 
+            onPress={this.placeSubmitHandler} />
+        </View>
+        <View>
+          {placesOutput}
         </View>
       </View>
     );

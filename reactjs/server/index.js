50e16aeb9751026e7223241d8da63023779e735f
Anthony Lee <antwonlee@gmail.com>
2018-05-04 10:57:31 -0500

    Relearn fullstack

--- reactjs/server/index.js
+++ reactjs/server/index.js
@@ -1,28 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20'),Strategy;
-const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accesToken) => {
-    console.log(accesToken);
-  })
-);
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-const PORT = process.env.PORT || 5000;
 app.listen(5000);

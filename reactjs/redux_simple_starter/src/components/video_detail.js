3c76056385f31c9dda82da1af099f29b16461e11
Anthony Lee <antwonlee@gmail.com>
2018-06-13 13:16:48 -0500

    Add video details

--- reactjs/redux_simple_starter/src/components/video_detail.js
+++ reactjs/redux_simple_starter/src/components/video_detail.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const VideoDetail = (props) => {
+const VideoDetail = ({video}) => {
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
   return(
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item"></iframe>
       </div>
       <div className="details">
-        <div></div>
-        <div></div>
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
       </div>
     </div>
   );

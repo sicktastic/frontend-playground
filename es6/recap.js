cf7c3e8300b48597a40272e303d781ed1a820d18
Anthony Lee <antwonlee@gmail.com>
2018-06-24 22:26:11 -0500

    Recap template literals

---
+++ es6/recap.js
@@ -0,0 +1,38 @@
+'use strict';
+
+console.log(productId);
+var productId = 12;
+// undefined
+
+console.log(productId);
+let productId = 12;
+// ReferenceError
+
+
+let productId = 12;
+{
+  let productId = 2000;
+}
+console.log(productId);
+// 12
+
+var getPrice = () => 5.99;
+console.log(typeof getPrice);
+
+///////////////////
+// Template literal
+
+const fruiteList = 
+  "<ul>" +
+    "<li>Kiwi</li>" +
+    "<li>Kiwi</li>" +
+    "<li>Kiwi</li>" +
+  "</ul>"
+
+const vegetableList = `
+  <ul>
+    <li>Potato</lo>
+    <li>Potato</lo>
+    <li>Potato</lo>
+  </ul>
+`;

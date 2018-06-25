4216644e94f4dfaf3f7d721ccaea2793e71a534c
Anthony Lee <antwonlee@gmail.com>
2018-06-25 14:39:44 -0500

    Add Lecture 1 notes

---
+++ react-native/cs50-mobile-app-development-with-react-native/lecture_1.js
@@ -0,0 +1,16 @@
+// 00:00:00 - ECMAScript
+// 00:03:35 - Closures
+// 00:13:47 - Immediately Invoked Function Expression
+// 00:22:53 - First-Class Functions
+// 00:34:19 - Synchronous? Async? Single-Threaded?
+// 00:40:07 - Asynchronous JavaScript
+// 00:40:18 - Execution Stack
+// 01:00:18 - Overflow
+// 01:01:47 - Asynchronous Functions
+// 01:03:05 - Callbacks
+// 01:09:41 - Promises
+// 01:20:57 - Async/Await
+// 01:26:32 - this
+// 01:43:48 - Browsers and the DOM
+//
+// https://goo.gl/wk9Ljm (Video Lecture)

24468028465ec6b3e12fd663b4b6a5e643e3b0cd
Anthony Lee <antwonlee@gmail.com>
2018-07-02 16:03:59 -0500

    Add React, Props, State

---
+++ react-native/cs50-mobile-app-development-with-react-native/lecture_2.js
@@ -0,0 +1,24 @@
+// React, Props, and State
+// 00:00:00 - Introduction
+// 00:00:56 - Classes
+// 00:04:25 - Set.js
+// 00:11:49 - extends
+// 00:14:22 - React
+// 00:15:12 - Imperative vs. Declarative
+// 00:21:14 - React is Declarative
+// 00:24:10 - React Components
+// 00:29:38 - React Performance
+// 00:31:07 - Writing React
+// 00:32:56 - Props
+// 00:34:12 - codesandbox.io
+// 00:40:43 - State
+// 00:56:04 - todoApp0.js
+// 00:58:27 - todoApp1.js
+// 00:59:23 - todoApp2.js
+// 01:00:53 - todoApp3.js
+// 01:04:32 - Todo List in React
+// 01:11:46 - addTodo
+// 01:18:26 - removeTodo
+// 01:26:10 - toggleTodo
+// 01:36:35 - React Native Teaser
+// https://goo.gl/BUCqYr

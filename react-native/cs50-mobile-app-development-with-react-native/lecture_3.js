db7081460865145af79ec999705a59473dbe2e86
Anthony Lee <antwonlee@gmail.com>
2018-07-20 09:51:00 -0500

    Add lecture 3

---
+++ react-native/cs50-mobile-app-development-with-react-native/lecture_3.js
@@ -0,0 +1,22 @@
+// https://goo.gl/ZDdNmk
+// 00:00:00 - Introduction
+// 00:00:56 - React Native
+// 00:02:38 - UI/JS Bridge
+// 00:04:04 - snack.expo.io
+// 00:07:52 - Differences Between React Native and the Web
+// 00:09:09 - React Native Components
+// 00:10:41 - todoApp4.js
+// 00:15:47 - Style
+// 00:21:17 - StyleSheet
+// 00:24:51 - Event Handling
+// 00:32:05 - Components
+// 00:33:15 - Stateless Functional Component (SFC)
+// 00:34:12 - React.Component
+// 00:36:42 - Component Lifecycle
+// 00:38:06 - Mount
+// 00:49:55 - Update
+// 00:58:20 - Unmount
+// 01:09:14 - Expo
+// 01:14:26 - Import/Export
+// 01:27:07 - PropTypes
+// 01:34:49 - How to Read Docs

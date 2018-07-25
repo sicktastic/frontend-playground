e1bb2045f4e86da99358bf02803d84b1c15cd11b
Anthony Lee <antwonlee@gmail.com>
2018-07-25 13:50:19 -0500

    Add style to header

--- react-native/albums/src/components/header.js
+++ react-native/albums/src/components/header.js
@@ -1,16 +1,36 @@
 // Import libraries for making a component
-
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 // Make a component
-
 const Header = () => {
-  return <Text>Albums</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
 };
 
+const styles = {
+  viewStyle: {
+    backgroundColor: '#f8f8f8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+}
 
 // Make the component available to other parts of the app
-
 export default Header;

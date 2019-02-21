1981073de4b08ba9fac3e04ba614de4d2a59be21
Anthony Lee <antwonlee@gmail.com>
2019-02-21 13:12:51 -0600

    Add quick lesson on props

--- reactjs/components/src/index.js
+++ reactjs/components/src/index.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">
-            Nice blog post!
-          </div>
-        </div>
-      </div>
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:45PM" 
+        content="Yo Yo Yo"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 2:00AM"
+        content="What What What"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Yesterday at 5:00PM"
+        content="Something Something"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };

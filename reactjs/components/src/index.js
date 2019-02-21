cb84986c8985bd198c681ed86899c998ae9bc707
Anthony Lee <antwonlee@gmail.com>
2019-02-21 13:26:45 -0600

    Add quick lesson on props children

--- reactjs/components/src/index.js
+++ reactjs/components/src/index.js
@@ -2,28 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail 
-        author="Sam" 
-        timeAgo="Today at 4:45PM" 
-        content="Yo Yo Yo"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Alex" 
-        timeAgo="Today at 2:00AM"
-        content="What What What"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail 
-        author="Jane" 
-        timeAgo="Yesterday at 5:00PM"
-        content="Something Something"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail 
+          author="Sam" 
+          timeAgo="Today at 4:45PM" 
+          content="Yo Yo Yo"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          author="Alex" 
+          timeAgo="Today at 2:00AM"
+          content="What What What"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

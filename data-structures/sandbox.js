e872dc671bcc822747261d131b72660a28c1e6e3
Anthony Lee <antwonlee@gmail.com>
2020-11-17 22:36:14 -0600

    Add data structuer teaching notes

---
+++ data-structures/sandbox.js
@@ -0,0 +1,14 @@
+// arrays and strings
+// ordered data, words
+
+// hash tables
+// optimization
+
+// linked lists
+// data insert/delet
+
+// stacks and queues
+// creative aux DS
+
+// trees, heaps
+// complex relationships
